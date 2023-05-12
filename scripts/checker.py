8177aec40af78e6492ff697322096266b31c10c8
Houssem Kraoua (ext.) <Houssem.Kraoua@partner.bmw.de>
2023-05-12 11:26:32 +0100

    fix job indexing issue and adapt handling script

--- scripts/checker.py
+++ scripts/checker.py
@@ -14,6 +14,13 @@ class RepoInfo:
     owner: str
     repo: str
 
+@dataclass
+class jobItem:
+    jobName: str
+    jobStatus: str
+    jobCompletion: str
+    jobApiIndex: int
+
 
 logger = Logger('Retry.checker')
 
@@ -24,22 +31,25 @@ def get_workflow_info() -> RepoInfo:
     repo = json_data['repository']['name']
     return RepoInfo(owner=owner, repo=repo)
 
-def extract_failed_jobs(data) -> List[str]:
-    job_names = []
+def extract_failed_jobs(data) -> List[jobItem]:
+    jobs_items = []
+    jobs = data["jobs"]
     while True:
-        for job in data["jobs"]:
+        for job in jobs:
             job_name = job["name"]
-            logger.info('Post job {}'.format(job_name))
-            if job_name != "retry-action" and job_name not in job_names:
-                job_names.append(job_name)
-        if all(job["status"] in ["completed", "failed", "cancelled"] for job in data["jobs"] if job["name"] != "retry-action"):
+            job_index = jobs.index(job)
+            logger.info('Post job {} with status {} and conclusion {}'.format(job_name))
+            job_item = jobItem(jobName=job_name, jobStatus=job["status"], jobCompletion=job["conclusion"], jobApiIndex=job_index)
+            if job_name != "retry-action" and job_name not in jobs_items:
+                jobs_items.append(job_item)
+        if all(job["status"] in ["completed", "failed", "cancelled"] for job in jobs if job["name"] != "retry-action"):
             break
-    return job_names
+    return jobs_items
 
 
-def extract_failed_steps_from_job(data, job_name):
+def extract_failed_steps_from_job(data, job_index):
     failure_count = 0
-    for step in data["jobs"][job_name]["steps"]:
+    for step in data["jobs"][job_index]["steps"]:
         if not step["name"].startswith("Set up") and not step["name"].startswith("Post"):
             step_name = step["name"]
             step_status = step["status"]
@@ -65,10 +75,10 @@ def check_workflow_api(token, inputs, api_url, run_id):
         else:
             logger.info('get API logs for workflow with ID {}'.format(run_id))
             data = json.loads(response.data)
-            failed_jobs = extract_failed_jobs(data)
-            logger.info('jobs {}'.format(failed_jobs))
-            for job_name in failed_jobs:
-                failed_steps_per_job = extract_failed_steps_from_job(data, job_name)
-                logger.info('steps per job {} : {}'.format(job_name, failed_steps_per_job))
+            jobs = extract_failed_jobs(data)
+            logger.info('jobs {}'.format(jobs))
+            for job in jobs:
+                failed_steps_per_job = extract_failed_steps_from_job(data, job.jobApiIndex)
+                logger.info('steps per job {} : {}'.format(job.jobName, failed_steps_per_job))
     except urllib3.exceptions.NewConnectionError:
         logger.error("Connection failed.")
