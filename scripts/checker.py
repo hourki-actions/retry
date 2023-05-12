d77dba60dba792ac67d8ed80a9d1dea1728a495e
Houssem Kraoua (ext.) <Houssem.Kraoua@partner.bmw.de>
2023-05-12 11:45:20 +0100

    cleanup logs and debug infos

--- scripts/checker.py
+++ scripts/checker.py
@@ -17,6 +17,7 @@ class RepoInfo:
 @dataclass
 class jobItem:
     jobName: str
+    jobRunId: str
     jobStatus: str
     jobCompletion: str
     jobApiIndex: int
@@ -38,8 +39,7 @@ def extract_failed_jobs(data) -> List[jobItem]:
         for job in jobs:
             job_name = job["name"]
             job_index = jobs.index(job)
-            logger.info('Post job {} with status {} and conclusion {}'.format(job_name, job["status"], job["conclusion"]))
-            job_item = jobItem(jobName=job_name, jobStatus=job["status"], jobCompletion=job["conclusion"], jobApiIndex=job_index)
+            job_item = jobItem(jobName=job_name, jobRunId=job["run_id"], jobStatus=job["status"], jobCompletion=job["conclusion"], jobApiIndex=job_index)
             if job_name != "retry-action" and job_name not in jobs_items:
                 jobs_items.append(job_item)
         if all(job["status"] in ["completed", "failed", "cancelled"] for job in jobs if job["name"] != "retry-action"):
@@ -52,10 +52,9 @@ def extract_failed_steps_from_job(data, job_index):
     for step in data["jobs"][job_index]["steps"]:
         if not step["name"].startswith("Set up") and not step["name"].startswith("Post"):
             step_name = step["name"]
-            step_status = step["status"]
             step_conclusion = step["conclusion"]
             logger.info(
-                'Job with name {} status {} conclusion {}'.format(step_name, step_status, step_conclusion))
+                'Step with name {} conclusion {}'.format(step_name, step_conclusion))
             if step_conclusion == "failure":
                 failure_count += 1
     return failure_count
@@ -76,9 +75,8 @@ def check_workflow_api(token, inputs, api_url, run_id):
             logger.info('get API logs for workflow with ID {}'.format(run_id))
             data = json.loads(response.data)
             jobs = extract_failed_jobs(data)
-            logger.info('jobs {}'.format(jobs))
             for job in jobs:
                 failed_steps_per_job = extract_failed_steps_from_job(data, job.jobApiIndex)
-                logger.info('steps per job {} : {}'.format(job.jobName, failed_steps_per_job))
+                logger.info('{} failed steps for job {}'.format(failed_steps_per_job, job.jobName))
     except urllib3.exceptions.NewConnectionError:
         logger.error("Connection failed.")
