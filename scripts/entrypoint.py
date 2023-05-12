8e9c90fa484466b40fe5011642a9fa1604acbd00
Houssem Kraoua (ext.) <Houssem.Kraoua@partner.bmw.de>
2023-05-12 02:55:05 +0100

    revert changes

--- scripts/entrypoint.py
+++ scripts/entrypoint.py
@@ -3,7 +3,7 @@ from checker import *
 
 if __name__ == "__main__":
     max_retries = os.environ["INPUT_MAX_RETRIES"]
-    repo_token = os.environ["TEST_TOKEN"]
+    repo_token = os.environ["INPUT_TOKEN"]
     runId = os.environ["INPUT_WORKFLOW_RUN_ID"]
     api_url = os.environ["GITHUB_API_URL"]
     repoInfo = get_workflow_info()
