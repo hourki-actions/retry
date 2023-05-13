ed8ff5192de13bdd9cf6863bf53e1f418abe6f24
Houssem Kraoua (ext.) <Houssem.Kraoua@partner.bmw.de>
2023-05-13 01:19:48 +0100

    set default workflow permission for the calling repo

--- scripts/builder.py
+++ scripts/builder.py
@@ -5,8 +5,11 @@ def build_url(api_url, owner, repo, action_path) -> str:
     return f"{api_url}/repos/{owner}/{repo}/{action_path}"
 
 
-def build_request(token, url, method):
+def build_request(token, url, method, body=None):
     headers = {
         'Authorization': f'Bearer {token}'
     }
-    return urllib3.request(method, url, headers=headers)
+    if body is None:
+      return urllib3.request(method, url, headers=headers)
+    else:
+      return urllib3.request(method, url, headers=headers, body=body)
--- scripts/retry.py
+++ scripts/retry.py
@@ -1,4 +1,5 @@
 import urllib3
+import orjson
 
 from actionTypes import types
 from logger import Logger
@@ -8,15 +9,23 @@ from builder import *
 logger = Logger('Retry.run')
 
 
-def get_defaukt_workflow_permissions(token, api_url, inputs):
+def get_default_workflow_permissions(token, api_url, inputs):
     url = build_url(api_url=api_url, owner=inputs.owner, repo=inputs.repo, action_path="actions/permissions/workflow")
     response = build_request(token=token, url=url, method="GET")
-    logger.info('Workflow default permissions {}'.format(response.status))
+    logger.info('Get Workflow default permissions {}'.format(response.status))
     print(response.data)
 
 
+def set_defaukt_workflow_permissions(token, api_url, inputs):
+    encoded_data = orjson.dumps({"default_workflow_permissions": "write"})
+    url = build_url(api_url=api_url, owner=inputs.owner, repo=inputs.repo, action_path="actions/permissions/workflow")
+    response = build_request(token=token, url=url, method="PUT", body=encoded_data)
+    logger.info('Set Workflow default permissions {}'.format(response.status))
+    print(response.data)
+
 def rerun_all_failed_jobs(run_id, api_url, inputs, token, job_name):
-    get_defaukt_workflow_permissions(token, api_url, inputs)
+    get_default_workflow_permissions(token, api_url, inputs)
+    set_defaukt_workflow_permissions(token, api_url, inputs)
     try:
         action_path = types.get_action_path('RERUN_ALL_FAILED_WORKFLOW_JOBS', run_id)
         url = build_url(api_url=api_url, owner=inputs.owner, repo=inputs.repo, action_path=action_path)
