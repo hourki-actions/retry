5f431095bcb2b6786efa15116d4307f8c53586e9
Houssem Kraoua (ext.) <Houssem.Kraoua@partner.bmw.de>
2023-05-14 02:18:04 +0100

    add graph to visualize failed steps

--- scripts/extract.py
+++ scripts/extract.py
@@ -1,5 +1,7 @@
 from typing import List
 from dataclasses import dataclass
+import networkx as nx
+import matplotlib.pyplot as plt
 
 from logger import Logger
 
@@ -16,18 +18,36 @@ class jobItem:
 logger = Logger('Retry.extract')
 
 
+def create_failed_steps_graph(data, job_index):
+    failed_steps = extract_failed_steps(data, job_index)
+    G = nx.DiGraph()
+    for step in failed_steps:
+        G.add_node(step)
+    for i, step in enumerate(failed_steps):
+        if i+1 < len(failed_steps):
+            G.add_edge(step, failed_steps[i+1])
+    return G
+
+
 def extract_steps_count_from_job(data, job_index, job_name):
     failure_count = 0
     skip_count = 0
+    G = nx.DiGraph()
     for step in data["jobs"][job_index]["steps"]:
         excluded_steps = ["Set up", "Post", "Complete job"]
         if not any(step["name"].startswith(name) for name in excluded_steps):
+            G.add_node(step)
+            for i, step in enumerate(data["jobs"][job_index]["steps"]):
+               if i+1 < len(data["jobs"][job_index]["steps"]):
+                 G.add_edge(step, data["jobs"][job_index]["steps"][i+1])
             step_conclusion = step["conclusion"]
             if step_conclusion == "failure":
                 failure_count += 1
             elif step_conclusion == "skipped":
                 skip_count += 1
     logger.info('{} failed / {} skipped step(s) for job "{}"'.format(failure_count, skip_count, job_name))
+    nx.draw(G, with_labels=True)
+    plt.show()
     return failure_count, skip_count
 
 
