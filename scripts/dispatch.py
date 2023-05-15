b8bcbc8a4e2c176f2db35b3a70e6a154884b9586
Houssem Kraoua (ext.) <Houssem.Kraoua@partner.bmw.de>
2023-05-15 15:58:31 +0100

    adapt json payload for dispatch

--- scripts/dispatch.py
+++ scripts/dispatch.py
@@ -16,15 +16,16 @@ def send_repo_dispatch_event(run_id, api_url, inputs, token, failed_jobs_ids):
             "event_type": "retry-event",
             "client_payload": {
                 "workflow_id": f"{run_id}",
-                "failed_job_ids": failed_jobs_ids,
-                "max_retries": 3
+                "failed_job_ids": f"{failed_jobs_ids}",
+                "max_retries": "3",
+                "from_dispatch": "true"
             }
         }
         response = build_request(token=token, url=url, method="POST", payload=payload)
         if response.status == 204:
             logger.info('Workflow Dispatch was triggered with {}'.format(response.status))
         else:
-            logger.error('Cannot trigger dispatch workflow failed job(s) with status {}'.format(response.status))
+            logger.error('Cannot trigger dispatch workflow with status {}'.format(response.status))
             logger.error(response.data)
     except urllib3.exceptions.NewConnectionError:
         logger.error("Connection failed.")
